fe5ede19e14022dbe81f82f3361eb8d34d8b8735
Esex <elseverclash57@gmail.com>
2023-11-26 22:20:52 +0400

    pustok with admin template is ready

---
+++ Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using IDONOTKNOW.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDONOTKNOW.Areas.Controllers;
+
+[Area("Admin")]
+public class DashboardController : Controller
+{
+    private readonly AppDbContext _dbContext;
+    public DashboardController(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Author()
+    {
+        return View();
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace IDONOTKNOW.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
